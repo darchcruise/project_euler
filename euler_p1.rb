ead5a255c3f33d54a7db9178a1b8cdb4830d4624
darchcruise <darchcruise@yahoo.com>
2014-01-12 14:16:38 -0500

    sums all the multiples of 3 or 5

--- euler_p1.rb
+++ euler_p1.rb
@@ -3,9 +3,40 @@
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-for i in 0..1000
-  if (i % 3 == 0) || (i % 5 == 0)
-    puts i
-    i += 1
-  end
+# list all the numbers that are muliples of 3 and 5
+# for i in 0..1000
+#   if (i % 3 == 0) || (i % 5 == 0)
+#     puts i
+#     i += 1
+#   end
+# end
+
+
+# sums up all the multiples of 3 and 5
+sum = 0
+for i in 0..999
+  sum += i if (i % 3 == 0) || (i % 5 == 0)
+    next
 end
+
+puts sum
+
+
+
+# sum = 0
+# (0..1000).each do |i|
+#   if (i % 3 == 0) || (i % 5 == 0)
+#     sum += i
+#     puts i
+#   end
+# end
+
+# (0..1000).inject(0) do |sum, i|
+#   sum += i if (i % 3 == 0) || (i % 5 == 0)
+# end
+
+# name = ["Jason", "Darcy"]
+
+# name.inject("") do |myname, element|
+#   myname << " #{element}"
+# end
